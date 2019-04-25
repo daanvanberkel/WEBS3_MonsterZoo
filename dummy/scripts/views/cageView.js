e63c00f1abe7bf7228305da9e58d2997746d0e59
Daan van Berkel <info@daanvanberkel.nl>
2019-04-25 22:30:26 +0200

    Obstakels toegevoegd

--- dummy/scripts/views/cageView.js
+++ dummy/scripts/views/cageView.js
@@ -2,19 +2,17 @@ import { EmptyTile } from "../models/emptyTile.js";
 
 export class CageView {
     drawCage(element, cage) {
-        // element.classList.add('tile');
-
-        // if (cage instanceof EmptyTile) {
+        if (cage instanceof EmptyTile) {
         //     element.style.background = 'blue';
-        //     element.classList.add('empty-tile');
-        //     return;
-        // }
+            element.classList.add('empty-tile');
+            return;
+        }
 
-        // if (!cage.roomForMonster) {
+        if (!cage.roomForMonster) {
         //     element.style.background = 'brown';
-        //     element.classList.add('obstacle');
+            element.classList.add('obstacle');
         // } else {
         //     element.style.background = 'green';
-        // }
+        }
     }
 }
