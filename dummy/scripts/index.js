1c4d125e1963573842a79faa925ef237cd9d75ae
Daan van Berkel <info@daanvanberkel.nl>
2019-05-15 12:46:44 +0200

    tile view veranderd naar tile component

--- dummy/scripts/index.js
+++ dummy/scripts/index.js
@@ -2,8 +2,10 @@
 
 import { ZooController } from "./controllers/zooController.js";
 import { MonsterConfiguratorComponent } from "./components/monsterConfigurator.js";
+import { TileComponent } from "./components/tile.js";
 
 window.customElements.define('monster-configurator', MonsterConfiguratorComponent);
+window.customElements.define('tile-component', TileComponent);
 
 const zooController = new ZooController();
 zooController.start('forest');
