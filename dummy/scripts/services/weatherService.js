22f434f408423b867a15120456c583db8ef8a3af
Daan van Berkel <info@daanvanberkel.nl>
2019-05-19 10:25:58 +0200

    Add method to override the weather

--- dummy/scripts/services/weatherService.js
+++ dummy/scripts/services/weatherService.js
@@ -1,13 +1,52 @@
 const api_key = "d9e90384775bc92904004bffe3c53aef";
 
 export class WeatherService {
+    constructor() {
+        this.weather = {};
+        this.useApi = true;
+
+        // In the javascript console use 'weatherControler.overrideApi()' to change the weather.
+        window.weatherControl = {
+            overrideApi: (raining, temp) => {
+                this.useApi = false;
+                this.weather = {
+                    raining: raining,
+                    temp: temp
+                };
+
+                console.log(`Set weather to ${JSON.stringify(this.weather)}`);
+            },
+
+            useApi: () => {
+                this.useApi = true;
+
+                console.log('Using API for weather data');
+            }
+        };
+    }
+
     getWeather(city) {
         return new Promise((resolve, reject) => {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`).then(response => {
-                return response.json();
-            }).then(response => {
-                resolve(response);
-            }).catch(reject);
+            if (this.useApi) {
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&lang=nl&units=metric`).then(response => {
+                    return response.json();
+                }).then(response => {
+                    let weather = {
+                        temp: response.main.temp
+                    };
+
+                    for(let w of response.weather) {
+                        if (w.main === "Rain") {
+                            weather.raining = true;
+                            break;
+                        }
+                    }
+
+                    resolve(weather);
+                }).catch(reject);
+            } else {
+                resolve(this.weather);
+            }
         });
     }
 }
